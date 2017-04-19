fdc5b5745632c13a732fc564324c25c8b06f103e
Deszip <raf.rafiki@gmail.com>
2017-04-19 13:11:20 +0300

    Removed layout fix

---
+++ ImageLoader.h
@@ -0,0 +1,15 @@
+//
+//  ImageLoader.h
+//  Reader
+//
+//  Created by Deszip on 19/04/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageLoader : NSObject
+
++ (UIImage *)loadImageNamed:(NSString *)imageName;
+
+@end
---
+++ ImageLoader.m
@@ -0,0 +1,18 @@
+//
+//  ImageLoader.m
+//  Reader
+//
+//  Created by Deszip on 19/04/2017.
+//
+//
+
+#import "ImageLoader.h"
+
+@implementation ImageLoader
+
++ (UIImage *)loadImageNamed:(NSString *)imageName {
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    return [UIImage imageNamed:imageName inBundle:bundle compatibleWithTraitCollection:nil];
+}
+
+@end
